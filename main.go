62696a1efd9282f598b28093e0d4a4d670911d66
Baigeiguai <1927405047@stu.suda.edu.cn>
2022-05-21 14:36:09 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Static("/static","static")
+	r.LoadHTMLGlob("templates/*")
+	
+	r.GET("/",func (c *gin.Context)  {
+		c.HTML(http.StatusOK,"index.html",nil)
+	})
+	r.Run(":9090")
+}
