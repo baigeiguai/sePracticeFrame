2c378a6418aab3136cebde8d696346e508bf0853
Baigeiguai <1927405047@stu.suda.edu.cn>
2022-05-23 17:18:43 +0800

    md5 encryption

--- utils/pswd2md5.go
+++ utils/pswd2md5.go
@@ -1,6 +1,12 @@
 package utils
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
 
-func Convert(password string )string{
-	return password
+func Convert(password string) string {
+	m := md5.New()
+	m.Write([]byte(password))
+	return hex.EncodeToString(m.Sum(nil))
 }
